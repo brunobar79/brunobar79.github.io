b0721ebfc885e26d27fb5efdad2641014e84db3b
Bruno Barbieri <brunobar79@gmail.com>
2015-06-05 03:04:53 -0400

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thehackerwaycoApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the thehackerwaycoApp
+ */
+angular.module('thehackerwaycoApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name thehackerwaycoApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the thehackerwaycoApp
+ */
+angular.module('thehackerwaycoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
