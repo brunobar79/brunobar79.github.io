c6138b955545d4c35bcc7782d4743693796b7451
Bruno Barbieri <brunobar79@gmail.com>
2015-09-07 17:55:59 -0400

    final spinner

--- app/scripts/directives/loader.js
+++ app/scripts/directives/loader.js
@@ -6,8 +6,11 @@ angular.module('website')
         function link(scope, element, attributes) {
             angular.element($window).bind('load', function() {
                 //Fadeout loader fast
-                element.children(0).children(0).fadeOut(1000, function(){
+                element.children(0).children(0).fadeOut('swing', function(){
                     //Fadeout bg
+                    //Start the animation of the clouds
+                    angular.element('#clouds').addClass('animate');
+
                     element.children(0).fadeOut(2000,function(){
                         element.remove();
                         scope = element = attributes = null;
