39e15afbd246034a64a40c95d3b1f7f383036de6
Bruno Barbieri <brunobar79@gmail.com>
2015-09-07 17:19:19 -0400

    added spinner

---
+++ app/scripts/directives/loader.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module('website')
+    .directive('appLoading', function( $window ) {
+        
+        function link(scope, element, attributes) {
+            angular.element($window).bind('load', function() {
+                //Fadeout loader fast
+                element.children(0).children(0).fadeOut(1000, function(){
+                    //Fadeout bg
+                    element.children(0).fadeOut(2000,function(){
+                        element.remove();
+                        scope = element = attributes = null;
+                    });
+                });                
+            });             
+        }
+
+        return({
+            restrict: 'C',
+            link: link            
+        });  
+
+    }
+);
